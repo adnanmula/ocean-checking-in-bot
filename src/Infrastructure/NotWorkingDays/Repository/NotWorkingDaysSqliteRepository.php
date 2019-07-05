acb163ae60945d51293096e98a73ff120214a1b1
adnan <adnanalfonso.mula@pccomponentes.com>
2019-07-05 20:28:48 +0200

    added sqlite db

---
+++ src/Infrastructure/NotWorkingDays/Repository/NotWorkingDaysSqliteRepository.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure\NotWorkingDays\Repository;
+
+use App\Infrastructure\SqliteRepository;
+
+class NotWorkingDaysSqliteRepository extends SqliteRepository
+{
+}
