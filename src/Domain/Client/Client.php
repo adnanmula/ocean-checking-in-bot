f72674f7dd80a7beab108a55c41300ac6ae5a7bb
adnan <adnanalfonso.mula@pccomponentes.com>
2019-07-08 08:22:51 +0200

    checkin request

--- src/Domain/Client/Client.php
+++ src/Domain/Client/Client.php
@@ -9,22 +9,28 @@ class Client extends \GuzzleHttp\Client
     private $baseUrl;
     private $user;
     private $password;
+    private $latitude;
+    private $longitude;
 
     public function __construct(
         string $baseUrl,
         string $user,
-        string $password
+        string $password,
+        float $latitude,
+        float $longitude
     ) {
         $this->baseUrl = $baseUrl;
         $this->user = $user;
         $this->password = $password;
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
 
-        parent::__construct([]);
+        parent::__construct(['base_uri' => $this->baseUrl]);
     }
 
     public function login(): \stdClass
     {
-        $login = $this->post($this->baseUrl . '/data/auth', [
+        $login = $this->post('/data/auth', [
             RequestOptions::JSON => [
                 'Login' => $this->user,
                 'Password' => $this->password,
@@ -45,7 +51,7 @@ class Client extends \GuzzleHttp\Client
 
     public function unlock(\stdClass $data): void
     {
-        $this->post($this->baseUrl . '/data/auth/unlock', [
+        $this->post('/data/auth/unlock', [
             RequestOptions::JSON => [
                 'usuarioId' => $data->Usuario->Id,
                 'tokenDesbloqueo' => $data->TokenDesbloqueo,
@@ -62,6 +68,24 @@ class Client extends \GuzzleHttp\Client
 
     public function checkIn(\stdClass $data)
     {
-        //TODO /data/marcajes/manual
+        $this->post(
+            '/data/marcajes/realizar-manual', [
+                RequestOptions::JSON => [
+                    'GeoLat' => $this->latitude,
+                    'GeoLong' => $this->longitude,
+                    'IncidenciaId' => null,
+                    'Nota' => null,
+                    'Tipo' => 'P',
+                    'TipoProd' => 1,
+                ],
+                RequestOptions::HEADERS => [
+                    'origin' => $this->baseUrl,
+                    'pragma' => 'no-cache',
+                    'referer' => $this->baseUrl,
+                    'user-agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+                    'authorization' => 'Bearer ' . $data->Token
+                ]
+            ]
+        );
     }
 }
