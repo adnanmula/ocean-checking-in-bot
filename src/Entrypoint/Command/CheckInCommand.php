81fb8370100535a68dc51ec05146d1c45cf2bde8
adnan <adnanalfonso.mula@pccomponentes.com>
2019-07-23 21:07:41 +0200

    added option to wait before checking-in

--- src/Entrypoint/Command/CheckInCommand.php
+++ src/Entrypoint/Command/CheckInCommand.php
@@ -7,6 +7,7 @@ use App\Domain\Notification\NotificationService;
 use App\Domain\NotWorkingDays\Repository\NotWorkingDaysRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class CheckInCommand extends Command
@@ -24,11 +25,22 @@ final class CheckInCommand extends Command
         parent::__construct();
     }
 
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add checkin')
+            ->addOption('random', 'r', InputOption::VALUE_OPTIONAL, 'Wait a random amount of minutes before checking in', false);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         if ($this->repository->check(new \DateTimeImmutable())) {
             $output->writeln('Today is a not working day.');
             return;
+        }
+
+        if (true === $input->hasParameterOption(['--random', '-r'])) {
+            sleep(mt_rand(0, 600));
         }
 
         $this->client->checkIn();
